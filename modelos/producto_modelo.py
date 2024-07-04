b8ecf10e7c09ce6e09366ab8c81f69ffb2b7d2ed
martin <tincho8919@gmail.com>
2024-07-04 17:55:23 -0300

    vamos

--- modelos/producto_modelo.py
+++ modelos/producto_modelo.py
@@ -8,7 +8,7 @@ class Producto(db.Model):   # la clase Producto hereda de db.Model
     nombre=db.Column(db.String(100))
     precio=db.Column(db.Integer)
     stock=db.Column(db.Integer)
-    tipo=db.column(db.String(100))
+    tipo=db.Column(db.String(100)) 
     imagen=db.Column(db.String(400))
     def __init__(self,nombre,precio,stock,tipo,imagen):   #crea el  constructor de la clase
         self.nombre=nombre   # no hace falta el id porque lo crea sola mysql por ser auto_incremento
